b41df7d3f3906e2589644be2ab0bf630c0963328
Eddie Perez Martell <eddie.perez1@upr.edu>
2015-04-17 01:08:43 -0400

    EddiePush

---
+++ EndoRiskWeb/Models/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EndoRiskWeb.Models
+{
+    public class IndexViewModel
+    {
+        public List<questions> quest {get; set;}
+        public List<symptom> symp {get; set;}
+
+       
+    }
+}
