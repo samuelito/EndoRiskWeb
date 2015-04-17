a3602c5c7abb11ab5599c29a5f094ce461235837
samuelito <samuel.feliciano@upr.edu>
2015-04-17 15:43:37 -0400

    Started Search

---
+++ EndoRiskWeb/Controllers/SearchResultsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EndoRiskWeb.Models;
+
+
+namespace EndoRiskWeb.Controllers
+{
+    public class SearchResultsController : Controller
+    {
+        private endoriskContext db = new endoriskContext();
+        
+
+        public ActionResult Index()
+        {
+            List<SearchResult> result = new List<SearchResult>();
+
+            SearchResult infoPaciente = new SearchResult();
+
+            infoPaciente.idQuiz = 1001;
+            infoPaciente.paciente = 1111;
+            infoPaciente.riesgo = (float) 56.32;
+            infoPaciente.severidad = "52.34";
+            infoPaciente.verificado = "Si";
+
+            foreach(var item in db.endoanswers)
+            {
+                if(item.idQuiz == 37){
+                    infoPaciente.datosPaciente.Add(item.answer);  
+                }
+            }
+
+            result.Add(infoPaciente);
+
+            return View(result);
+        }
+    }
+}
---
+++ EndoRiskWeb/Models/SearchResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EndoRiskWeb.Models
+{
+    public class SearchResult
+    {
+        public int idQuiz { get; set; }
+
+        public int paciente { get; set; }
+
+        public string verificado { get; set; }
+
+        public float riesgo { get; set; }
+
+        public string severidad { get; set; }
+
+        public IList<string> datosPaciente { get; set; }
+    }
+}
