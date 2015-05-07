c1d5ec3ff326321eccd3bbffc5bcd6487ac63b9a
samuelito <samuel.feliciano@upr.edu>
2015-05-07 16:07:51 -0400

    Partial Integration Two

---
+++ EndoRiskWeb/Models/patientsprecond.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EndoRiskWeb.Models
+{
+    public class patientsprecond
+    {
+        public int idCondition { get; set; }
+        public int idQuiz { get; set; }
+        public string preCondition { get; set; }
+        public string preAbbr { get; set; }
+        public int haspreCond { get; set; }
+    }
+}
---
+++ EndoRiskWeb/Models/searchdata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EndoRiskWeb.Models
+{
+    public class searchdata
+    {
+
+        public patient datosPaciente { get; set; }
+
+        public IList<endoanswer> respuestasPaciente { get; set; }
+
+        public IList<patientsymptom> sintomasPaciente { get; set; }
+    }
+}
