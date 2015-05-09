d93735f918bf5c60e158184216bff8aa1644392a
Eddie Perez Martell <eddie.perez1@upr.edu>
2015-05-09 19:03:15 -0400

    Proyecto_7:02pm

--- EndoRiskWeb/Controllers/C_Sharp_RExcel.cs
+++ EndoRiskWeb/Controllers/C_Sharp_RExcel.cs
@@ -16,211 +16,159 @@ using Microsoft.Internal;
 using EndoRiskWeb.Models;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Threading;
 
 namespace EndoRiskWeb.Controllers
 {
     public class C_Sharp_RExcel
     {
-             public object[] Prediction(object[,] answerList)
-             {
-                //Make sure excel version is an US version
-                System.Globalization.CultureInfo oldCI;
-                oldCI = System.Threading.Thread.CurrentThread.CurrentCulture;
-                System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+        public object[] Prediction(object[,] answerList)
+        {
+            //Make sure excel version is an US version
+            System.Globalization.CultureInfo oldCI;
+            oldCI = System.Threading.Thread.CurrentThread.CurrentCulture;
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
 
-                //Define your Excel Application Objects
-                Excel.Application excelApp = null;
-                Excel._Workbook excelWorkbook = null;
-                Excel._Worksheet excelWorksheet = null;
-                //Excel.Range excelRange = null;
-                Excel.Range excelLine = null;
-               
-                //Start Excel Application and open the workbook.
-                excelApp = new Excel.Application();
-                excelApp.Visible = false;
-                excelApp.DisplayAlerts = false;
-                                
-                //Open Prediction Excel Workbook
-                excelWorkbook = (Excel._Workbook)excelApp.Workbooks.Open("C:\\Users\\eddie.perez\\Desktop\\Prediction.xlsm", Missing.Value, ReadOnly: false);
-               
+            //Define your Excel Application Objects
+            Excel.Application excelApp = null;
+            Excel._Workbook excelWorkbook = null;
+            Excel._Worksheet excelWorksheet = null;
+            Excel.Range excelLine = null;
 
-                //Enable RExcel Add-In
-                Excel.AddIn ad = (Excel.AddIn)excelApp.AddIns.get_Item(1);
-                ad.Installed = true;
+            //Start Excel Application and open the workbook.
+            excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            excelApp.DisplayAlerts = false;
 
-                excelWorkbook = excelApp.ActiveWorkbook;
-                excelWorksheet = excelApp.ActiveSheet as Excel._Worksheet;
-            
-                //Fill each cell with user answer
-                excelWorksheet = (Excel.Worksheet)excelWorkbook.Worksheets.get_Item(2);
-                excelWorksheet.Activate();         
-                excelWorksheet.Columns.ClearFormats();
-                excelWorksheet.Rows.ClearFormats();
-                excelLine = excelWorksheet.Cells;
-                excelLine = excelWorksheet.Rows[excelWorksheet.UsedRange.Rows.Count];
-                excelLine.Activate();
-              
-                int rowOffset = 0;
-                for (int i = 0; i <= excelWorksheet.Columns.Count- 1; i++)
-                    {
-                        if (excelWorksheet.Cells[1, i + 1].Value() == null )
-                        {
-                            break;
-                        }
+            //Open Prediction Excel Workbook
+            excelWorkbook = (Excel._Workbook)excelApp.Workbooks.Open("C:\\Users\\eddie.perez\\Desktop\\Prediction.xlsm", Missing.Value, ReadOnly: false);
 
-                        else if(excelWorksheet.Cells[1, i + 1].Value() == "Y")
-                        {
-                            excelWorksheet.Cells[(excelWorksheet.UsedRange.Rows.Count + 1) - rowOffset, i + 1] = 0;
-                          
-                            break;
-                        }
 
-                        for (int j = 0; j <= answerList.GetLength(0);j++ )
-                        {                           
-                            if (excelWorksheet.Cells[1, i + 1].Value().ToString() == answerList[j, 1].ToString())
-                            {
-                                
-                                excelWorksheet.Cells[(excelWorksheet.UsedRange.Rows.Count + 1) - rowOffset, i + 1] = answerList[j, 0];
-                             
-                                if (rowOffset == 0) { rowOffset++; }
-                                
-                                break;
-                            }
-                        }
-                    }
+            //Enable RExcel Add-In
+            Excel.AddIn ad = (Excel.AddIn)excelApp.AddIns.get_Item(1);
+            ad.Installed = true;
 
-                //Open Severity Excel Workbook
-                excelWorkbook = (Excel._Workbook)excelApp.Workbooks.Open("C:\\Users\\eddie.perez\\Desktop\\Severity_Data.csv", Missing.Value, ReadOnly: false);
-                
-                excelWorkbook = excelApp.ActiveWorkbook;
-                excelWorksheet = excelApp.ActiveSheet as Excel._Worksheet;
-                excelWorksheet = (Excel.Worksheet)excelWorkbook.Worksheets.get_Item(1);
-                excelWorksheet.Activate();
-                excelWorksheet.Columns.ClearFormats();
-                excelWorksheet.Rows.ClearFormats();
-                excelLine = excelWorksheet.Cells;
-                excelLine = excelWorksheet.Rows[excelWorksheet.UsedRange.Rows.Count];
-                excelLine.Activate();
+            excelWorkbook = excelApp.ActiveWorkbook;
+            excelWorksheet = excelApp.ActiveSheet as Excel._Worksheet;
 
-                rowOffset = 0;
-                for (int i = 0; i <= excelWorksheet.Columns.Count - 1; i++)
+            //Fill each cell with user answer
+            excelWorksheet = (Excel.Worksheet)excelWorkbook.Worksheets.get_Item(2);
+            excelWorksheet.Activate();
+            excelLine = excelWorksheet.Cells;
+            excelLine.Activate();
+
+            for (int i = 0; i <= excelWorksheet.Columns.Count - 1; i++)
+            {
+                if (excelWorksheet.Cells[1, i + 1].Value() == null)
                 {
-                    if (excelWorksheet.Cells[1, i + 1].Value() == null)
+                    break;
+                }
+
+                else if (excelWorksheet.Cells[1, i + 1].Value() == "Y")
+                {
+                    excelWorksheet.Cells[2, i + 1] = 0;
+
+                    break;
+                }
+
+                for (int j = 0; j <= answerList.GetLength(0); j++)
+                {
+                    if (excelWorksheet.Cells[1, i + 1].Value().ToString() == answerList[j, 1].ToString())
                     {
+
+                        excelWorksheet.Cells[2, i + 1] = answerList[j, 0];
+
                         break;
                     }
+                }
+            }
 
-                    else if (excelWorksheet.Cells[1, i + 1].Value() == "Severity")
+            excelWorksheet = (Excel.Worksheet)excelWorkbook.Worksheets.get_Item(3);
+            excelWorksheet.Activate();
+            excelLine = excelWorksheet.Cells;
+            excelLine.Activate();
+
+            for (int i = 0; i <= excelWorksheet.Columns.Count - 1; i++)
+            {
+                if (excelWorksheet.Cells[1, i + 1].Value() == null)
+                {
+                    break;
+                }
+
+                else if (excelWorksheet.Cells[1, i + 1].Value() == "Severity")
+                {
+                    excelWorksheet.Cells[2, i + 1] = "I-II";
+
+                    break;
+                }
+
+                for (int j = 0; j <= answerList.GetLength(0); j++)
+                {
+                    if (excelWorksheet.Cells[1, i + 1].Value().ToString() == answerList[j, 1].ToString())
                     {
-                        excelWorksheet.Cells[(excelWorksheet.UsedRange.Rows.Count + 1) - rowOffset, i + 1] = "I-II";
-                     
+                        excelWorksheet.Cells[2, i + 1] = answerList[j, 0];
+
                         break;
                     }
-
-                    for (int j = 0; j <= answerList.GetLength(0); j++)
-                    {
-                        if (excelWorksheet.Cells[1, i + 1].Value().ToString() == answerList[j, 1].ToString())
-                        {
-                            excelWorksheet.Cells[(excelWorksheet.UsedRange.Rows.Count + 1) - rowOffset, i + 1] = answerList[j, 0];
-                            
-                            if (rowOffset == 0) { rowOffset++; }
-                            break;
-                        }
-                    }
-                }            
-
-                //Run the macro in Excel that run scripts in R workbench
-                RunMacro(excelApp, new Object[] { "Prediction.xlsm!Prediction.Prediction" });        
-                 
-                //Get the prediction value from excel sheet     
-                excelWorkbook = excelApp.Workbooks[1];
-                excelWorkbook = excelApp.ActiveWorkbook;
-                excelWorksheet = (Excel.Worksheet)excelWorkbook.Worksheets.get_Item(1);
-                excelWorksheet.Activate();
-
-                object lifetime_risk_value = excelWorksheet.Cells[2, 1].Value;
-                object severity = excelWorksheet.Cells[2, 2].Value;
-                object [] test_results = new object[2];
-                test_results[0] = (double)lifetime_risk_value;
-                test_results[1] = (string)severity;
-
-                //Save and close current Excel Workbook
-                excelWorkbook.Close(true);
-
-                excelApp.Quit();
-               // Marshal.FinalReleaseComObject(excelApp);
-                //Quit the Excel Application and releaese all Excel application objects used in the program from memory
-                if (excelApp != null)
-                {
-                    KillExcelFileProcess("Prediction", "R Console 32-bit");                    
-                    releaseObject(excelWorksheet);                    
-                    releaseObject(excelWorkbook);
-                    releaseObject(excelApp);
-                    GC.Collect();
-                    GC.WaitForPendingFinalizers();
-                    GC.Collect();
-                    GC.WaitForPendingFinalizers();
                 }
-
-                return test_results; 
             }
-            
-            //Method to relase all Excel application objects used
-            private void releaseObject(object obj)
+
+            //Run the macro in Excel that run scripts in R workbench
+            excelApp.Run("Prediction.xlsm!Prediction.Prediction");
+
+            excelWorksheet = (Excel.Worksheet)excelWorkbook.Worksheets.get_Item(1);
+            excelWorksheet.Activate();
+
+            object lifetime_risk_value = excelWorksheet.Cells[2, 1].Value;
+            object severity = excelWorksheet.Cells[2, 2].Value;
+            object[] test_results = new object[2];
+            test_results[0] = (double)lifetime_risk_value;
+            test_results[1] = (string)severity;
+
+            excelApp.Run("CloseExcel");
+
+            //Quit the Excel Application and releaese all Excel application objects used in the program from memory
+            if (excelApp != null)
             {
-                try
-                {
-                    System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
-                    obj = null;
-                }
-
-                catch (Exception)
-                {
-                    obj = null;
-                }
-
-                finally
-                {
-                    GC.Collect();
-                    GC.WaitForPendingFinalizers();
-                    GC.Collect();
-                    GC.WaitForPendingFinalizers();
-                }
+                KillExcelFileProcess();
+                releaseObject(excelWorksheet);
+                releaseObject(excelWorkbook);
+                releaseObject(excelApp);
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
             }
 
-           
-                
-            //    xlApp.DisplayAlerts = false;
-            //    xlApp.AskToUpdateLinks = false;
-         
+            return test_results;
+        }
 
-            //Method to run Excel macro
-            private void RunMacro(object oApp, object[] oRunArgs)
+        //Method to relase all Excel application objects used
+        private void releaseObject(object obj)
+        {
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+            obj = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        //Method to end the process executing Excel
+        private void KillExcelFileProcess()
+        {
+            var excel_process = from proc in System.Diagnostics.Process.GetProcessesByName("EXCEL") select proc;
+
+            foreach (var process in excel_process)
             {
-                   oApp.GetType().InvokeMember("Run", System.Reflection.BindingFlags.Default | System.Reflection.BindingFlags.InvokeMethod,
-                   null, oApp, oRunArgs);
+                process.Kill();
             }
+            var R_process = from proc in System.Diagnostics.Process.GetProcessesByName("Rgui") select proc;
 
-            //Method to end the process executing Excel
-            private void KillExcelFileProcess(string excelFileName, string R_Program)
+            foreach (var process in R_process)
             {
-                
-                var excelProcess = from proc in System.Diagnostics.Process.GetProcessesByName("EXCEL") select proc;
-
-                foreach (var process in excelProcess)
-                {
-                    if (process.MainWindowTitle == "Excel")
-                        process.Kill();
-                }
-
-                var R_process = from proc in System.Diagnostics.Process.GetProcessesByName("Rgui") select proc;
-
-                foreach (var process in R_process)
-                {
-                    if (process.MainWindowTitle == "R Console (32-bit)")
-                        process.Kill();
-                }
+                if (process.MainWindowTitle == "R Console (32-bit)")
+                    process.Kill();
             }
-	}
+        }
+    }
 }
